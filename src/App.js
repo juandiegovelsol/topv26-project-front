7b82ddcfac709e5483aff0a8a6b9c788431a2af2
Juan <juandiegovelsol@gmail.com>
2023-02-04 16:50:25 -0500

    Creates Menu component, button Menu displaying side bar menu

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import solar_roof_image from "./resources/images/solar-roof.webp";
 import accessories_image from "./resources/images/accessories.jfif";
 import "./App.css";
 import { TeslaArticle } from "./resources/components/TeslaArticle.js";
+import { Menu } from "./resources/components/Menu.js";
 import React, { useState } from "react";
 const data = [
   {
@@ -68,6 +69,19 @@ const data = [
 ];
 
 function App() {
+  const [menuCoverClass, setMenuCoverClass] = useState(
+    "menuCoverPage displayNone"
+  );
+  function showFunc() {
+    let auxCoverClass = menuCoverClass;
+    if (auxCoverClass === "menuCoverPage displayNone") {
+      auxCoverClass = "menuCoverPage";
+    } else {
+      auxCoverClass = "menuCoverPage displayNone";
+    }
+    setMenuCoverClass(auxCoverClass);
+  }
+
   return (
     <div className="App">
       <header className="headerWrapper">
@@ -92,24 +106,36 @@ function App() {
           <article className="optionsIndex">
             <button className="shopButton">Shop</button>
             <button className="accountButton">Account</button>
-            <button className="menuButton">Menu</button>
+            <button
+              className="menuButton"
+              onClick={() => setMenuCoverClass("menuCoverPage")}
+            >
+              Menu
+            </button>
           </article>
         </section>
       </header>
       <main className="mainWrapper">
-        {data.map((item) => {
-          console.log(item.key);
-          return (
-            <TeslaArticle
-              keys={item.key}
-              titleText={item.articleTitle}
-              additionalText={item.text}
-              source={item.source}
-              button1={item.button1}
-              button2={item.button2}
-            />
-          );
-        })}
+        <section className="articleContentWrapper">
+          {data.map((item) => {
+            console.log(item.key);
+            return (
+              <TeslaArticle
+                keys={item.key}
+                titleText={item.articleTitle}
+                additionalText={item.text}
+                source={item.source}
+                button1={item.button1}
+                button2={item.button2}
+              />
+            );
+          })}
+        </section>
+
+        <Menu
+          menuCoverClasss={menuCoverClass}
+          setMenuCoverClasss={setMenuCoverClass}
+        />
       </main>
       <footer className="footerWrapper"></footer>
     </div>
--- src/resources/components/TeslaArticle.js
+++ src/resources/components/TeslaArticle.js
@@ -17,17 +17,17 @@ export function TeslaArticle(props) {
     }
   };
   return (
-    <section className={setScrollSnapFeature(keys)} key={keys}>
+    <article className={setScrollSnapFeature(keys)} key={keys}>
       <h2 className="articleTitle">{titleText}</h2>
       <p className="h2LowerText">{additionalText}</p>
-      <article className="mainBackgroundImageWrapper">
-        <img src={source} className="mainBackgroundImage" alt="Article image" />
-      </article>
+      <span className="mainBackgroundImageWrapper">
+        <img src={source} className="mainBackgroundImage" alt="Article" />
+      </span>
 
-      <article className="articleButtons">
+      <span className="articleButtons">
         <button className="customOrderButton">{button1}</button>
         <button className={setClassFn(button2)}>{button2}</button>
-      </article>
-    </section>
+      </span>
+    </article>
   );
 }
