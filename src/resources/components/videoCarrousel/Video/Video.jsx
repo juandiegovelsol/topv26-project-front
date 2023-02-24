a83b2f249c10747a8f9d4187e6a9dc570a43940a
Juan <juandiegovelsol@gmail.com>
2023-02-23 23:07:35 -0500

    Video Carrousel improved, now autoplays

--- src/resources/components/videoCarrousel/Video/Video.jsx
+++ src/resources/components/videoCarrousel/Video/Video.jsx
@@ -5,7 +5,7 @@ import "./video.scss";
 const Video = ({ videoArticles, selector }) => {
   return (
     <article className="videoContainer">
-      <video key={videoArticles[selector].key} controls autoplay muted>
+      <video key={videoArticles[selector].key} autoplay="true" muted loop>
         <source src={videoArticles[selector].video} type="video/mp4" />
       </video>
     </article>
