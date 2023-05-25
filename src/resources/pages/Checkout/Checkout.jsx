6036ed09fff65be3f6675065c577035e633ea104
Juan <juandiegovelsol@gmail.com>
2023-05-25 17:27:21 -0500

    Feat: checkout page develop, user must be logged to access this page, dynamic page

---
+++ src/resources/pages/Checkout/Checkout.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { selecOrder } from "../order/orderSlice";
+
+import "./checkout.scss";
+import { CustomHeader } from "../../components/customHeader";
+
+const Checkout = () => {
+  const { checkout } = useSelector(selecOrder);
+  const { modelSelector, imageSelector } = checkout || {};
+  return (
+    <section className="checkout">
+      <div className="checkout__container">
+        <aside className="checkout__images">
+          <img src={imageSelector.img} alt="car" />
+          <span className="checkout__price-resume">
+            <p>
+              <strong>
+                ${modelSelector.price + imageSelector.aditional_price} Vehicle
+                Price
+              </strong>
+            </p>
+            <p>{` | `}</p>
+            <p>
+              ${modelSelector.price + imageSelector.aditional_price - 6000}{" "}
+              After Potential Savings
+            </p>
+          </span>
+        </aside>
+        <aside className="checkout__resume">
+          <h2>Checkout</h2>
+          <h3>{modelSelector.title}</h3>
+          <p>Est. Delivery: June 2023</p>
+          <span className="checkout__info">
+            <div>
+              <h4>
+                <span>{modelSelector.range}</span> mi
+              </h4>
+              <p>Range {`(EPA est.)`}</p>
+            </div>
+            <div>
+              <h4>
+                <span>{modelSelector.speed}</span> mph
+              </h4>
+              <p>Top Speed</p>
+            </div>
+            <div>
+              <h4>
+                <span>{modelSelector.time}</span> Sec
+              </h4>
+              <p>0-60 mph</p>
+            </div>
+          </span>
+          <span className="checkout__paint">
+            <h4>Paint</h4>
+            <p>{imageSelector.title}</p>
+          </span>
+          <span className="checkout__order">
+            <h4>Order your {modelSelector.title}</h4>
+            <button onClick={/* handlePayment */ () => {}}>
+              Continue To Payment
+            </button>
+          </span>
+        </aside>
+      </div>
+    </section>
+  );
+};
+
+export default Checkout;
